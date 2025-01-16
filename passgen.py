7a79c814b67bb1aa07a1bc8a06db223ef192aae6
steve <mariscurrenalae@gmail.com>
2025-01-15 20:17:34 -0600

    Adding the password generator function (genpass(

--- passgen.py
+++ passgen.py
@@ -1,3 +1,11 @@
 ## Password generator
 ## Author: ANgel Mariscurrena
 ## Inspo: W J Pearce
+
+import random
+import string as str
+
+def genpass(lenght: int = 10): #If argument is not set, the utility set a default parameter of ten
+    alphabet = str.ascii_letters + str.digits + str.punctuation ##Store all the ascii letters, all the digits and all the punctuation signs into the US and UK system
+    pwd = ''.join(random.choice(alphabet) for i in range(lenght)) ##Randomly choose a character of the alphabet array defined before for every caracter of the password (use lenght value) and concatenate this to a blank string, setting in this way a random password
+    return pwd #Return password
